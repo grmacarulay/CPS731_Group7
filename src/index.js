7c8b5a820bdf8dd1c7e0260f20a98a8070e8610b
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-11-19 19:49:45 -0500

    Revert "Update index.js"
    This reverts commit 2c69ad8a913e39764bfd31e049c445da6e8f2cf0.

--- src/index.js
+++ src/index.js
@@ -105,14 +105,11 @@ function handleSignOut() {
 }
 
 function addDatabase() {
-  var getuid = window.localStorage.getItem("UID")
   db.collection("users").add({
-    id: getuid,
-    first_name: "Alan",
-    last_name: "Mathison",
+    first: "Alan",
+    middle: "Mathison",
     last: "Turing",
     born: 1912
-
   })
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
